d9a34964dfb3ded2b460bafceb737a71e745e9c5
Stefan Rinke <sker65@gmail.com>
2015-03-17 21:02:41 +0100

    - which prop to use

--- src/main/java/com/rinke/solutions/pinball/GoDMDClock.java
+++ src/main/java/com/rinke/solutions/pinball/GoDMDClock.java
@@ -175,7 +175,7 @@ public class GoDMDClock {
     
     public static void main(String[] args) {
         Display display = new Display();
-        String filename = "animations.properties";
+        String filename = "animations3.properties";
         if( args.length>0 ) filename = args[0];
         new GoDMDClock(display, filename);
         display.dispose();
