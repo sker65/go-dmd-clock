595c546b84908c03d64b13d61f97943d872c3d63
Stefan Rinke <sker65@gmail.com>
2015-05-08 21:34:21 +0200

    - added font resources

---
+++ fonts/asimov/copy-mask.sh
@@ -0,0 +1,15 @@
+cp Image-0x016A.png Image-0x016A-mask.png
+cp Image-0x016B.png Image-0x016B-mask.png
+cp Image-0x016C.png Image-0x016C-mask.png
+cp Image-0x016D.png Image-0x016D-mask.png
+cp Image-0x016E.png Image-0x016E-mask.png
+cp Image-0x016F.png Image-0x016F-mask.png
+cp Image-0x0170.png Image-0x0170-mask.png
+cp Image-0x0171.png Image-0x0171-mask.png
+cp Image-0x0172.png Image-0x0172-mask.png
+cp Image-0x0173.png Image-0x0173-mask.png
+cp Image-0x0174.png Image-0x0174-mask.png
+cp Image-0x0175.png Image-0x0175-mask.png
+cp Image-0x0176.png Image-0x0176-mask.png
+cp Image-0x0177.png Image-0x0177-mask.png
+cp Image-0x0178.png Image-0x0178-mask.png
