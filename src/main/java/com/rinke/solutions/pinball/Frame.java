2e503ee60ea9fa31fd4afe0b071cc74dc5eabb79
Stefan Rinke <sker65@gmail.com>
2015-07-30 02:41:27 +0200

    - added ctor with w,h

--- src/main/java/com/rinke/solutions/pinball/Frame.java
+++ src/main/java/com/rinke/solutions/pinball/Frame.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 public class Frame {
     
-    public Frame(int delay) {
+    public Frame(int delay,int w, int h) {
         this.delay = delay;
-    }
+        width = w;
+        height = h;
+   }
     
     public Frame(int w, int h, byte[] plane1, byte[] plane2) {
         width = w;
@@ -24,6 +26,16 @@ public class Frame {
     @Override
     public String toString() {
         return "Frame [delay=" + delay + ", planes=" + planes + "]";
+    }
+
+    public void setPixel(int x, int y) {
+        if( x>=0 && x< width && y>=0 && y<height) {
+            int bytesPerRow = width / 8;
+            //System.out.println("setPixel("+x+","+y+")");
+            int offset = y*bytesPerRow + x / 8;
+            planes.get(0).plane[offset] &= ~ (128>>x%8);
+            planes.get(1).plane[offset] &= ~ (128>>x%8);
+        }
     }
     
 }
