4af0d0a368895a616b8f6950fb260633691a4ee2
Stefan Rinke <stefanri@Stefans-MacBook-Pro.local>
2015-11-06 14:18:26 +0100

    writer test

--- src/main/java/com/rinke/solutions/pinball/model/PalMapping.java
+++ src/main/java/com/rinke/solutions/pinball/model/PalMapping.java
@@ -4,26 +4,26 @@ import java.util.Arrays;
 
 public class PalMapping {
     public byte digest[];
-    public Palette palette;
+    public int palIndex;
     public long durationInMillis;
     public int durationInFrames;
     
-    public PalMapping(byte[] digest, Palette palette, long durationInMillis, int durationInFrames) {
+    public PalMapping(byte[] digest, int palIndex, long durationInMillis, int durationInFrames) {
         super();
         if( digest.length != 16 ) {
             throw new IllegalArgumentException("digest length must be 16");
         }
         this.digest = new byte[digest.length];
         System.arraycopy(digest, 0, this.digest, 0, 16);
-        this.palette = palette;
+        this.palIndex = palIndex;
         this.durationInMillis = durationInMillis;
         this.durationInFrames = durationInFrames;
     }
 
 	@Override
 	public String toString() {
-		return "PalMapping [digest=" + Arrays.toString(digest) + ", palette="
-				+ palette + ", durationInMillis=" + durationInMillis
+		return "PalMapping [digest=" + Arrays.toString(digest) + ", palIndex="
+				+ palIndex + ", durationInMillis=" + durationInMillis
 				+ ", durationInFrames=" + durationInFrames + "]";
 	}
     
