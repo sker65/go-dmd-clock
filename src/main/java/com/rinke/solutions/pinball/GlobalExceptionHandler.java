d8e988005c13383b9d2541dc9f3c971750181c4e
Stefan Rinke <sker65@gmail.com>
2015-11-21 22:58:19 +0100

    improve error dialog

--- src/main/java/com/rinke/solutions/pinball/GlobalExceptionHandler.java
+++ src/main/java/com/rinke/solutions/pinball/GlobalExceptionHandler.java
@@ -1,6 +1,14 @@
 package com.rinke.solutions.pinball;
 
+import java.util.ArrayList;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.MultiStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
@@ -42,16 +50,50 @@ public class GlobalExceptionHandler {
             
             @Override
             public void run() {
-                MessageBox messageBox = new MessageBox(shell,
-                        SWT.ICON_WARNING | SWT.YES | SWT.NO );
-                
-                messageBox.setText("Fehler aufgetreten!");
-                messageBox.setMessage(getMessage(e)+"\n\nTrotzdem weitermachen?");
-                if( SWT.NO == messageBox.open()) System.exit(1);
+                MultiStatus status = createMultiStatus(e.getLocalizedMessage(), e);
+                ErrorDialog errorDialog = new ErrorDialog(Display.getCurrent().getActiveShell(),
+                        "Error", "Ein unerwarteter Fehler ist aufgetreten", status,
+                        IStatus.OK | IStatus.INFO | IStatus.WARNING | IStatus.ERROR) {
+                    protected void createButtonsForButtonBar(Composite parent) {
+                        createButton(parent, IDialogConstants.ABORT_ID, IDialogConstants.ABORT_LABEL, true);
+                        createButton(parent, IDialogConstants.PROCEED_ID, IDialogConstants.PROCEED_LABEL, false);
+                        createDetailsButton(parent);
+                    }
+
+                    @Override
+                    protected void buttonPressed(int id) {
+                        super.buttonPressed(id);
+                        if( id == IDialogConstants.ABORT_ID || id == IDialogConstants.PROCEED_ID) {
+                            setReturnCode(id);
+                            close();
+                        }
+                    }
+                    
+                };
+                int ret = errorDialog.open();
+                if( ret == IDialogConstants.ABORT_ID ) System.exit(1);
+                System.out.println(ret); // cancel = 1
                 lastException=null;
             }
         });
     }
+    
+    private static MultiStatus createMultiStatus(String msg, Throwable t) {
+
+        java.util.List<Status> childStatuses = new ArrayList<>();
+        StackTraceElement[] stackTraces = t.getStackTrace();
+
+        for (StackTraceElement stackTrace : stackTraces) {
+            Status status = new Status(IStatus.ERROR, "com.rinke.solutions.pinball", stackTrace.toString());
+            childStatuses.add(status);
+        }
+
+        MultiStatus ms = new MultiStatus("com.rinke.solutions.pinball", IStatus.ERROR, childStatuses.toArray(new Status[] {}),
+                t.toString(), t);
+        return ms;
+    }
+
+
 
 
     public Display getDisplay() {
