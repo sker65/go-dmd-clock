e94bb09ab19e882fce97b3e523d99480061b4668
Stefan Rinke <stefanri@Stefans-MacBook-Pro.local>
2015-11-27 16:59:10 +0100

    buid dist with version

--- build-dist.sh
+++ build-dist.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
+version=$(git describe --always --tags | awk -F- '{print $1}')
 rm -rf build/
 mkdir build
 mvn -Pwin clean package assembly:single launch4j:launch4j
-cp target/pin2dmd-editor.exe build/
+cp target/pin2dmd-editor.exe build/pin2dmd-editor-${version}.exe
 mvn -Pwin_64 clean package assembly:single launch4j:launch4j
-cp target/pin2dmd-editor64.exe build/
+cp target/pin2dmd-editor64.exe build/pin2dmd-editor64-${version}.exe
 mvn -Plinux_x86_64 clean package assembly:single
-cat dist/stub.sh target/go-dmd-clock-*-jar-with-dependencies.jar >pin2dmd-editor && chmod a+x pin2dmd-editor
-mv pin2dmd-editor build/
+cat dist/stub.sh target/go-dmd-clock-*-jar-with-dependencies.jar >pin2dmd-editor-${version} && chmod a+x pin2dmd-editor-${version}
+mv pin2dmd-editor-${version} build/
 mvn -Pmac_64 clean package assembly:single
 cd dist
 rm -rf Pin2Dmd-Editor.app/
 ant
-zip -r Pin2Dmd-Editor.zip Pin2Dmd-Editor.app
+zip -r Pin2Dmd-Editor64-${version}.zip Pin2Dmd-Editor.app
 cd ..
-mv dist/Pin2Dmd-Editor.zip build
+mv dist/Pin2Dmd-Editor64-${version}.zip build
 mvn -Pmac clean package assembly:single
 cd dist
 rm -rf Pin2Dmd-Editor.app/
 ant
-zip -r Pin2Dmd-Editor32.zip Pin2Dmd-Editor.app
+zip -r Pin2Dmd-Editor-${version}.zip Pin2Dmd-Editor.app
 cd ..
-mv dist/Pin2Dmd-Editor32.zip build
+mv dist/Pin2Dmd-Editor-${version}.zip build
 echo "Successfull build"
 
