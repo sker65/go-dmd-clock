3975c5d6fce28ef6f34468c24cc464006fa78d17
Stefan Rinke <stefanri@Stefans-MBP.fritz.box>
2015-12-08 22:22:05 +0100

    pal imprt tests

--- src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
+++ src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
@@ -2,39 +2,57 @@ package com.rinke.solutions.pinball;
 
 import java.util.List;
 
+import org.eclipse.swt.widgets.Shell;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import com.fappel.swt.DisplayHelper;
+
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PinDmdEditorTest {
+	
 	@Mock
 	private List<Animation> animations;
-	
+
 	@InjectMocks
 	private PinDmdEditor pinDmdEditor = new PinDmdEditor();
-	
+
 	@Test
 	public void testCutScene() throws Exception {
-		Animation src = new Animation(AnimationType.MAME, "./src/test/resources/drwho-dump.txt.gz", 0, 100, 0, 0, 0);
-		Animation cutScene = pinDmdEditor.cutScene(src , 10, 20);
-		assertThat( cutScene, notNullValue() );
-		assertThat( cutScene.end - cutScene.start, equalTo(10) );
-		
+		Animation src = new Animation(AnimationType.MAME,
+				"./src/test/resources/drwho-dump.txt.gz", 0, 100, 0, 0, 0);
+		Animation cutScene = pinDmdEditor.cutScene(src, 10, 20);
+		assertThat(cutScene, notNullValue());
+		assertThat(cutScene.end - cutScene.start, equalTo(10));
+
 		src.actFrame = 10;
-		Frame srcFrame = src.render(new DMD(128,32), false);
+		Frame srcFrame = src.render(new DMD(128, 32), false);
 		cutScene.actFrame = 0;
-		Frame destFrame = cutScene.render(new DMD(128,32), false);
-		assertThat( srcFrame.planes.size(), equalTo(destFrame.planes.size()) );
+		Frame destFrame = cutScene.render(new DMD(128, 32), false);
+		assertThat(srcFrame.planes.size(), equalTo(destFrame.planes.size()));
 		int i = 0;
 		for (Plane p : srcFrame.planes) {
-			assertThat( p.plane, equalTo(destFrame.planes.get(i++).plane) );
+			assertThat(p.plane, equalTo(destFrame.planes.get(i++).plane));
 		}
-		
+
 	}
+
+	@Rule
+	public final DisplayHelper displayHelper = new DisplayHelper();
+
+//	@Test
+//	public void testMouseDownIncreasesCount() {
+//		Shell shell = displayHelper.createShell();
+//		pinDmdEditor.createContents(shell);
+//		//trigger(SWT.MouseDown).on(control);
+//		//assertEquals(1, counter.getCount());
+//	}
 
 }
--- src/test/java/com/rinke/solutions/pinball/SmartDMDImporterTest.java
+++ src/test/java/com/rinke/solutions/pinball/SmartDMDImporterTest.java
@@ -10,10 +10,12 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.swt.graphics.RGB;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.rinke.solutions.pinball.model.Palette;
+import com.rinke.solutions.pinball.model.PaletteType;
 
 
 public class SmartDMDImporterTest {
@@ -45,6 +47,23 @@ public class SmartDMDImporterTest {
         assertEquals(15, p.size());
     }
     
+    @Test
+    public void testImportFromFile3() throws Exception {
+        List<Palette> list = uut.importFromFile("SmartDmd2.txt");
+        assertEquals(52, list.size());
+        Palette p = list.get(0);
+        assertEquals("Default", p.name);
+        assertEquals(new RGB(0,0,0), p.colors[0]);
+        assertEquals(new RGB(0x4a,0x4a,0x4a), p.colors[1]);
+        p = list.get(4);
+        assertEquals("Sparky MB Intro 1", p.name);
+        assertEquals(new RGB(0,0,0), p.colors[0]);
+        assertEquals(new RGB(0x4a,0x4a,0x4a), p.colors[1]);
+        assertEquals(PaletteType.PERSISTENT, p.type);
+        
+        assertEquals(PaletteType.DEFAULT, list.get(11).type);
+    }
+
     String pal = "Ship,Upscaling=2,0xFF000000,0xFFFFFFBB,0xFF804000,0xFF808000,0xFFAC0000,0xFF797979,0xFFAC5400,0xFFACACAC,0xFF545454,0xFF5454FF,0xFF0000FF,0xFF000080,0xFFFF5454,0xFF494925,0xFFFFFF54,0xFFFFFFFF";
     
     @Test
