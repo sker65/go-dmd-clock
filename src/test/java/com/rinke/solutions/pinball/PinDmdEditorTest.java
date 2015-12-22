df73dba91a4717c1ac8bfd5e8a53ff1d7d7b06aa
Stefan Rinke <stefanri@Stefans-MacBook-Pro.local>
2015-12-22 15:14:19 +0100

    added export tests

--- src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
+++ src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
@@ -10,6 +10,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
+import com.rinke.solutions.pinball.model.PalMapping;
+
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertNull;
@@ -19,6 +21,8 @@ public class PinDmdEditorTest {
 	
 	PinDmdEditor uut = new PinDmdEditor();
 	
+	byte[] digest = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+	
 	@Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
 
@@ -26,6 +30,25 @@ public class PinDmdEditorTest {
 	public void testReplaceExtensionTo() throws Exception {
 		String newName = uut.replaceExtensionTo("ani", "foo.xml");
 		assertThat(newName, equalTo("foo.ani"));
+	}
+	
+	@Test
+	public void testExportProjectWithMapping() throws Exception {
+		
+		File tempFile = testFolder.newFile("test.dat");
+		String filename = tempFile.getAbsolutePath();
+		
+		PalMapping p = new PalMapping(0, "foo");
+		p.digest = digest;
+		
+		uut.project.palMappings.add(p );
+		
+		uut.exportProject(filename);
+		
+		//System.out.println(filename);
+
+		// create a reference file and compare against
+		assertNull( isBinaryIdentical( filename, "./src/test/resources/palettesOneMapping.dat"));
 	}
 
 	@Test
@@ -44,9 +67,9 @@ public class PinDmdEditorTest {
 	private String isBinaryIdentical(String filename, String filename2) throws IOException {
 		byte[] b1 = IOUtils.toByteArray(new FileInputStream(filename));
 		byte[] b2 = IOUtils.toByteArray(new FileInputStream(filename2));
-		if( b1.length != b2.length ) return String.format("different lenth %i : %i", b1.length, b2.length);
+		if( b1.length != b2.length ) return String.format("different lenth %d : %d", b1.length, b2.length);
 		for( int i = 0; i < b1.length; i++) {
-			if( b1[i] != b2[i] ) return String.format("files differ at %i", i);
+			if( b1[i] != b2[i] ) return String.format("files differ at %d", i);
 		}
 		return null;
 	}
