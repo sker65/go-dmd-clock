6945ffee133a0ddee8ed429fd54352f5a981a183
Stefan Rinke <stefanri@Stefans-MacBook-Pro.local>
2015-12-22 14:55:09 +0100

    export test

--- src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
+++ src/test/java/com/rinke/solutions/pinball/PinDmdEditorTest.java
@@ -1,13 +1,18 @@
 package com.rinke.solutions.pinball;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class PinDmdEditorTest {
@@ -24,15 +29,26 @@ public class PinDmdEditorTest {
 	}
 
 	@Test
-	public void testExportProjectString() throws Exception {
+	public void testExportProjectEmpty() throws Exception {
+		
 		File tempFile = testFolder.newFile("test.dat");
 		String filename = tempFile.getAbsolutePath();
-		uut.exportProject(filename);
 		
-		assertTrue(new File(filename).exists());
+		uut.exportProject(filename);
+		//System.out.println(filename);
 
 		// create a reference file and compare against
-		
+		assertNull( isBinaryIdentical( filename, "./src/test/resources/defaultPalettes.dat"));
+	}
+
+	private String isBinaryIdentical(String filename, String filename2) throws IOException {
+		byte[] b1 = IOUtils.toByteArray(new FileInputStream(filename));
+		byte[] b2 = IOUtils.toByteArray(new FileInputStream(filename2));
+		if( b1.length != b2.length ) return String.format("different lenth %i : %i", b1.length, b2.length);
+		for( int i = 0; i < b1.length; i++) {
+			if( b1[i] != b2[i] ) return String.format("files differ at %i", i);
+		}
+		return null;
 	}
 
 }
