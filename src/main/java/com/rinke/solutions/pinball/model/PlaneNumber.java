4be12315144c4aecf7feb279e2034f33910a6645
Stefan Rinke <sker65@gmail.com>
2015-12-24 14:43:19 +0100

    - new model for plane number

---
+++ src/main/java/com/rinke/solutions/pinball/model/PlaneNumber.java
@@ -0,0 +1,19 @@
+package com.rinke.solutions.pinball.model;
+
+public enum PlaneNumber {
+	two(2), four(4);
+	
+	PlaneNumber(int i) {
+		this.numberOfPlanes = i;
+	}
+	
+	public int numberOfPlanes;
+
+	public static PlaneNumber valueOf(int i) {
+		PlaneNumber[] values = values();
+		for (int j = 0; j < values.length; j++) {
+			if( values[j].numberOfPlanes == i ) return values[j];
+		}
+		return null;
+	}
+}
