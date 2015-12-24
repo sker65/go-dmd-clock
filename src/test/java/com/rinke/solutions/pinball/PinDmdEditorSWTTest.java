a91da88ece3e61714ac038d99360e48aa161ea35
Stefan Rinke <sker65@gmail.com>
2015-12-24 10:34:27 +0100

    - added test for keyframe list change

--- src/test/java/com/rinke/solutions/pinball/PinDmdEditorSWTTest.java
+++ src/test/java/com/rinke/solutions/pinball/PinDmdEditorSWTTest.java
@@ -4,21 +4,32 @@ import static com.fappel.swt.SWTEventHelper.trigger;
 import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.Matchers.*;
 
+import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.SelectionChangedEvent;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.rules.Verifier;
+import org.junit.runner.Description;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import com.fappel.swt.DisplayHelper;
+import com.fappel.swt.JFaceViewerHelper;
+import com.fappel.swt.SWTEventHelper;
+import com.rinke.solutions.pinball.animation.AniEvent;
 import com.rinke.solutions.pinball.animation.Animation;
 import com.rinke.solutions.pinball.animation.AnimationType;
+import com.rinke.solutions.pinball.animation.CompiledAnimation;
+import com.rinke.solutions.pinball.animation.EventHandler;
 import com.rinke.solutions.pinball.model.PalMapping;
 import com.rinke.solutions.pinball.model.Palette;
 
+import static com.fappel.swt.JFaceViewerHelper.fireSelectionChanged;
 
 @RunWith(MockitoJUnitRunner.class)
 public class PinDmdEditorSWTTest {
@@ -31,19 +42,59 @@ public class PinDmdEditorSWTTest {
 
 	Shell shell;
 	
+	public EventHandler eventHandler = new EventHandler() {
+		
+		@Override
+		public void notifyAni(AniEvent evt) {
+			
+		}
+	};
+	
 	@Before
 	public void setup() {
 		shell = displayHelper.createShell();
 		uut.createContents(shell);	
 		uut.createNewProject();
 		
-		uut.animationHandler = new  AnimationHandler(null,null,null,false);
+		DMD dmd = new DMD(128,32);
+		uut.animationHandler = new  AnimationHandler(null,null,dmd ,false);
+		uut.animationHandler.setScale(uut.scale);
+		uut.animationHandler.setEventHandler(eventHandler);
 		
 		uut.createBindings();
 		
 		byte[] digest = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
 		uut.hashes.add(digest);
 
+	}
+	
+	@Rule
+	public Verifier verifier= new Verifier() {
+
+		@Override
+		protected void verify() throws Throwable {
+			GlobalExceptionHandler handler = GlobalExceptionHandler.getInstance();
+			Exception exception = handler.getLastException();
+			if( exception != null ) {
+				handler.setLastException(null);
+				throw exception;
+			}
+		}
+	      
+	};
+	
+	@Test
+	public void testSelectKeyFrame() throws Exception {
+		uut.shell = shell;
+		PalMapping palMapping = new PalMapping(0,"foo");
+		palMapping.animationName = "drwho-dump";
+		palMapping.frameIndex = 0;
+		
+		uut.loadAni("./src/test/resources/drwho-dump.txt.gz", false, true);
+		
+		SelectionChangedEvent e = new SelectionChangedEvent(uut.keyframeListViewer, 
+				new StructuredSelection(palMapping));
+		fireSelectionChanged(uut.keyframeListViewer, e);
 	}
 	
 	@Test
