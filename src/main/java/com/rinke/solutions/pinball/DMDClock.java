232fa56943343eac233eeaa0dd241eae146cf0a8
Stefan Rinke <sker65@gmail.com>
2016-01-07 11:15:40 +0100

    - org imports

--- src/main/java/com/rinke/solutions/pinball/DMDClock.java
+++ src/main/java/com/rinke/solutions/pinball/DMDClock.java
@@ -9,7 +9,6 @@ import java.io.InputStream;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Properties;
 
 import org.slf4j.Logger;
