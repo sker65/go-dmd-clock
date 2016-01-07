7b0fd68048717eadaf0bbd86edbab7bb61979c96
Stefan Rinke <sker65@gmail.com>
2016-01-07 11:19:18 +0100

    - formatting

--- src/main/java/com/rinke/solutions/pinball/renderer/VPinMameRenderer.java
+++ src/main/java/com/rinke/solutions/pinball/renderer/VPinMameRenderer.java
@@ -19,62 +19,65 @@ import com.rinke.solutions.pinball.animation.Frame;
 // die helligkeits schwellen angebbar sein
 
 public class VPinMameRenderer extends Renderer {
-    
-    private static Logger LOG = LoggerFactory.getLogger(VPinMameRenderer.class); 
 
-   @Override
-    public long getTimeCode(int actFrame) {
-        return actFrame< frames.size() ? frames.get(actFrame).timecode:0;
-    }
+	private static Logger LOG = LoggerFactory.getLogger(VPinMameRenderer.class);
 
-   void readImage(String filename, DMD dmd) {
+	@Override
+	public long getTimeCode(int actFrame) {
+		return actFrame < frames.size() ? frames.get(actFrame).timecode : 0;
+	}
+
+	void readImage(String filename, DMD dmd) {
 		BufferedReader stream = null;
 		int frameNo = 0;
 		int timecode = 0;
-		int lastTimeStamp=0;
+		int lastTimeStamp = 0;
 		try {
-			stream = new BufferedReader( new InputStreamReader(
-					new GZIPInputStream(new FileInputStream(new File(filename)))));
+			stream = new BufferedReader(
+					new InputStreamReader(new GZIPInputStream(
+							new FileInputStream(new File(filename)))));
 			String line = stream.readLine();
-			Frame res = new Frame(dmd.getWidth(), dmd.getHeight(), 
-					new byte[dmd.getFrameSizeInByte()], new byte[dmd.getFrameSizeInByte()]);
+			Frame res = new Frame(dmd.getWidth(), dmd.getHeight(),
+					new byte[dmd.getFrameSizeInByte()],
+					new byte[dmd.getFrameSizeInByte()]);
 
-			int j=0;
-			while(line!=null) {
-				if( line.startsWith("0x")) {
+			int j = 0;
+			while (line != null) {
+				if (line.startsWith("0x")) {
 					int newTs = Integer.parseInt(line.substring(2), 16);
-					if( frameNo>0 && lastTimeStamp >0) {
-					    frames.get(frameNo-1).delay = (int) (newTs - lastTimeStamp);
-	                    timecode += (newTs - lastTimeStamp);
-					    res.timecode = timecode;
+					if (frameNo > 0 && lastTimeStamp > 0) {
+						frames.get(frameNo - 1).delay = (int) (newTs - lastTimeStamp);
+						timecode += (newTs - lastTimeStamp);
+						res.timecode = timecode;
 					}
 					lastTimeStamp = newTs;
 					line = stream.readLine();
 					continue;
 				}
-				if( line.length()==0 ) {
+				if (line.length() == 0) {
 					frames.add(res);
 					frameNo++;
-					res = new Frame(dmd.getWidth(), dmd.getHeight(), 
-							new byte[dmd.getFrameSizeInByte()], new byte[dmd.getFrameSizeInByte()]);
-					LOG.debug("reading frame: "+frameNo);
+					res = new Frame(dmd.getWidth(), dmd.getHeight(),
+							new byte[dmd.getFrameSizeInByte()],
+							new byte[dmd.getFrameSizeInByte()]);
+					LOG.debug("reading frame: " + frameNo);
 					j = 0;
 					line = stream.readLine();
 					continue;
 				}
-				for( int i=0; i<line.length();i++) {
+				for (int i = 0; i < line.length(); i++) {
 					char c = line.charAt(i);
 					int bit = (i % 8);
-					int b = i/8;
+					int b = i / 8;
 					int mask = 128 >> bit;
-					if( c == '1') {
-						res.planes.get(0).plane[j+b] |= mask;
-					} else if( c == '2') {
-						res.planes.get(1).plane[j+b] |= mask; 
-					} else if( c == '3') {
-						res.planes.get(0).plane[j+b] |= mask;
-						res.planes.get(1).plane[j+b] |= mask;
-						
+					if (c == '1') {
+						res.planes.get(0).plane[j + b] |= mask;
+					} else if (c == '2') {
+						res.planes.get(1).plane[j + b] |= mask;
+					} else if (c == '3') {
+						res.planes.get(0).plane[j + b] |= mask;
+						res.planes.get(1).plane[j + b] |= mask;
+
 					}
 				}
 				j += 16;
@@ -83,7 +86,7 @@ public class VPinMameRenderer extends Renderer {
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
-			if( stream != null )
+			if (stream != null)
 				try {
 					stream.close();
 				} catch (IOException e) {
