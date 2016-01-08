accd3a4fd12aa51bcc27828cddcd4321139526ed
Stefan Rinke <sker65@gmail.com>
2016-01-08 13:22:10 +0100

    - remove double frame model

--- src/main/java/com/rinke/solutions/pinball/model/Frame.java
+++ src/main/java/com/rinke/solutions/pinball/model/Frame.java
@@ -2,46 +2,95 @@ package com.rinke.solutions.pinball.model;
 
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import com.rinke.solutions.pinball.animation.Plane;
+import com.rinke.solutions.pinball.model.Model;
 
 public class Frame implements Model {
-	public int timecode;
-	public List<byte[]> planes = new ArrayList<>();
-	
-	public Frame(int timecode, List<byte[]> planes) {
-		super();
-		this.timecode = timecode;
-		this.planes = planes;
-	}
+    
+    public int delay;
+    public int timecode;
+    public List<Plane> planes = new ArrayList<>();
 
-	//TODO remove different models
-	public Frame(com.rinke.solutions.pinball.animation.Frame frame) {
-		this.timecode = frame.delay;
-		for(Plane p: frame.planes) {
-		    byte[] transformed = com.rinke.solutions.pinball.animation.Frame.transform(p.plane);
-			planes.add(transformed);
+    public Frame(Frame src) {
+    	this.delay = src.delay;
+    	this.timecode = src.timecode;
+    	for (Plane p : src.planes) {
+			planes.add(new Plane(p.marker, p.plane));
 		}
 	}
 
-	@Override
-	public String toString() {
-		return "Frame [timecode=" + timecode + ", planes=" + planes + "]";
+	public Frame(byte[] plane1, byte[] plane2) {
+        planes.add(new Plane((byte)0, plane1));
+        planes.add(new Plane((byte)1, plane2));
+    }
+
+    public Frame( byte[] plane1, byte[] plane2, byte[] plane3, byte[] plane4) {
+        planes.add(new Plane((byte)0, plane1));
+        planes.add(new Plane((byte)1, plane2));
+        planes.add(new Plane((byte)2, plane3));
+        planes.add(new Plane((byte)3, plane4));
+    }
+
+    public Frame() {
 	}
+
+	public List<byte[]> getHashes() {
+        List<byte[]> res = new ArrayList<>();
+        try {
+            int j = 0;
+            
+            StringBuilder sb = new StringBuilder();
+            for (Plane plane : planes) {
+                MessageDigest md = MessageDigest.getInstance("MD5");
+                md.update(transform(plane.plane), 0, plane.plane.length);
+                res.add(md.digest());
+            }
+            return res;
+        } catch (NoSuchAlgorithmException e) {
+         
+        }
+        return res;
+    }
+
+    public static byte[] transform(byte[] plane) {
+    	return transform(plane, 0, plane.length);
+    }
+
+	public static byte[] transform(byte[] data, int offset, int size) {
+        byte[] res = new byte[size];
+        for(int i = offset; i < size+offset; i++) {
+            byte x = data[i];
+            byte b = 0;
+            for( int bit=0; bit<8; bit++){
+                b<<=1;
+                b|=( x &1);
+                x>>=1;
+            }
+            res[i-offset]=b;
+        }
+        return res;
+	}
+
+    @Override
+    public String toString() {
+        return "Frame [delay=" + delay + ", timecode=" + timecode + ", planes="
+                + planes + "]";
+    }
 
 	@Override
 	public void writeTo(DataOutputStream os) throws IOException {
-		os.writeInt(timecode);
+		os.writeInt(delay);
 		os.writeShort(planes.size());
 		if( !planes.isEmpty() ) {
-            os.writeShort(planes.get(0).length);
+            os.writeShort(planes.get(0).plane.length);
 		}
-		for(byte[] data : planes) {
-			os.write(data);
+		for(Plane p : planes) {
+			os.write(p.plane);
 		}
 	}
-	
+
 }
