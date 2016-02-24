8cba865d336e52f38edae65a911dd10a075bb5cc
Stefan Rinke <sker65@gmail.com>
2016-02-24 08:29:27 +0100

    - moved to api project

--- src/main/java/com/rinke/solutions/pinball/model/DefaultPalette.java
+++
@@ -1,5 +0,0 @@
-package com.rinke.solutions.pinball.model;
-
-public enum DefaultPalette {
-    Vga, Red, Green, Blue, Pink, Cyan, Yellow, Grey, 
-}
--- src/main/java/com/rinke/solutions/pinball/model/DeviceMode.java
+++
@@ -1,5 +0,0 @@
-package com.rinke.solutions.pinball.model;
-
-public enum DeviceMode {
-    PinMameRGB, PinMameMono, WPC, Stern, 
-}
