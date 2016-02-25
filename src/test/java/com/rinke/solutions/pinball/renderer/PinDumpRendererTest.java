f298e02443761f5b3aa1c1e4f6c907f4ef24be3c
Stefan Rinke <sker65@gmail.com>
2016-02-25 13:01:02 +0100

    new commit mit unit test für renderer

---
+++ src/test/java/com/rinke/solutions/pinball/renderer/PinDumpRendererTest.java
@@ -0,0 +1,22 @@
+package com.rinke.solutions.pinball.renderer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.rinke.solutions.pinball.DMD;
+
+public class PinDumpRendererTest {
+	
+	String base = "./src/test/resources/renderer/";
+	
+	PinDumpRenderer uut = new PinDumpRenderer();
+
+	@Test
+	public void testReadImage() throws Exception {
+		DMD dmd = new DMD(128,32);
+		uut.readImage(base+"real_tz.dump", dmd );
+		assertEquals(632, uut.getFrames().size());
+	}
+
+}
