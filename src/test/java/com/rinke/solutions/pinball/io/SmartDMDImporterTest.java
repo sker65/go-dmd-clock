0177fbeb49631ca6b695da6688296bae67f8b9fd
Stefan Rinke <sker65@gmail.com>
2016-04-17 16:41:42 +0200

    - org imports

--- src/test/java/com/rinke/solutions/pinball/io/SmartDMDImporterTest.java
+++ src/test/java/com/rinke/solutions/pinball/io/SmartDMDImporterTest.java
@@ -6,8 +6,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.List;
 
 import org.junit.Before;
