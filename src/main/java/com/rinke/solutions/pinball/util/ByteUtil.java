a82e6e11c25003a93ffc71fc7c883092ab7590c3
Stefan Rinke <sker65@gmail.com>
2017-04-17 22:31:11 +0200

    added bit shift util for byte arrays

--- src/main/java/com/rinke/solutions/pinball/util/ByteUtil.java
+++ src/main/java/com/rinke/solutions/pinball/util/ByteUtil.java
@@ -4,8 +4,9 @@ public class ByteUtil {
 	
 	/**
 	 * Shifts input byte array len bits left.This method will alter the input byte array.
+	 * @param positivePadding 
 	 */
-	public static byte[] shiftLeft(byte[] data, int len) {
+	public static byte[] shiftLeft(byte[] data, int len, boolean positivePadding) {
 	    int word_size = (len / 8) + 1;
 	    int shift = len % 8;
 	    byte carry_mask = (byte) ((1 << shift) - 1);
@@ -13,10 +14,13 @@ public class ByteUtil {
 	    for (int i = 0; i < data.length; i++) {
 	        int src_index = i+offset;
 	        if (src_index >= data.length) {
-	            data[i] = 0;
+	            data[i] = (byte) (positivePadding ? -1 : 0);
 	        } else {
 	            byte src = data[src_index];
 	            byte dst = (byte) (src << shift);
+	            if( src_index == data.length-1 && positivePadding ) {
+	            	dst |= 255 >> (8-shift);
+	            }
 	            if (src_index+1 < data.length) {
 	                dst |= data[src_index+1] >>> (8-shift) & carry_mask;
 	            }
@@ -26,7 +30,13 @@ public class ByteUtil {
 	    return data;
 	}
 	
-	public static byte[] shiftRight(byte[] data, int len) {
+	public static byte[] shift(byte[] data, int shift, boolean positivePadding) {
+		if( shift>0 ) return shiftRight(data, shift, positivePadding);
+		if( shift<0 ) return shiftLeft(data, -shift, positivePadding);
+		return data;
+	}
+	
+	public static byte[] shiftRight(byte[] data, int len, boolean positivePadding) {
 	    int word_size = (len / 8) + 1;
 	    int shift = len % 8;
 	    byte carry_mask =(byte) (0xFF >>> shift);
@@ -35,10 +45,13 @@ public class ByteUtil {
 	    for (int i = data.length-1; i >= 0; i--) {
 	        int src_index = i-offset;
 	        if (src_index < 0) {
-	            data[i] = 0;
+	            data[i] = (byte) (positivePadding ? -1 : 0);
 	        } else {
 	            byte src = data[src_index];
 	            byte dst = (byte) (Byte.toUnsignedInt(src) >>> shift);
+	            if( src_index == 0 && positivePadding ) {
+	            	dst |= 255 << (8-shift);
+	            }
 	            if (src_index-1 >= 0 ) {
 	                dst |= data[src_index-1] << (8-shift) & carry_mask;
 	            }
