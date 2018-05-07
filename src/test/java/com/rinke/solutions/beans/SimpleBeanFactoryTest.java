3a23d532a8ebcff1bdb1f32dadb362ae504f4905
Stefan Rinke <sker65@gmail.com>
2018-05-07 20:04:48 +0200

    - improved some test

--- src/test/java/com/rinke/solutions/beans/SimpleBeanFactoryTest.java
+++ src/test/java/com/rinke/solutions/beans/SimpleBeanFactoryTest.java
@@ -1,5 +1,7 @@
 package com.rinke.solutions.beans;
 
+import static org.junit.Assert.assertEquals;
+
 import java.lang.reflect.Field;
 
 import org.junit.Before;
@@ -8,6 +10,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.AttributesImpl;
 
 import com.rinke.solutions.beans.BeanFactory.PropertyProvider;
 import com.rinke.solutions.pinball.util.Config;
@@ -31,6 +35,12 @@ public class SimpleBeanFactoryTest {
 	@Test(expected=RuntimeException.class)
 	public void testGetBeansOfType() throws Exception {
 		uut.getBeanByType(String.class);
+	}
+
+	@Test
+	public void testGetBeansOfTypeWithSingleton() throws Exception {
+		uut.setSingleton("foo", new String("foo"));
+		uut.getBeansOfType(String.class);
 	}
 
 	@Test(expected=RuntimeException.class)
@@ -59,6 +69,33 @@ public class SimpleBeanFactoryTest {
 	public void testGetValueForField() throws Exception {
 		Field f = this.getClass().getDeclaredField("testField");
 		uut.getValueForField(f );
+	}
+
+	@Test
+	public void testGetBeannameFromMethodName() throws Exception {
+		assertEquals("foo",uut.getBeannameFromMethodName("getFoo"));
+		assertEquals("foo",uut.getBeannameFromMethodName("createFoo"));
+	}
+	
+	@Test
+	public void testStartElement() throws Exception {
+		AttributesImpl attributes = new AttributesImpl();
+		attributes.addAttribute("uri", "locl", "id", "string", "foo");
+		attributes.addAttribute("uri", "locl", "class", "string", "java.lang.Integer");
+		attributes.addAttribute("uri", "locl", "value", "string", "1");
+		uut.startElement("uri", "foo", "bean", attributes );
+	}
+	
+	public static class TestObject {
+		@Autowired public Integer foo;
+	}
+
+	@Test
+	public void testInject() throws Exception {
+		TestObject obj = new TestObject();
+		uut.setSingleton("foo", Integer.valueOf(1));
+		uut.inject(obj);
+		assertEquals(Integer.valueOf(1), obj.foo);
 	}
 
 }
