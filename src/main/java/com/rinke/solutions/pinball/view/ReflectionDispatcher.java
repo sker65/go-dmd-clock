862dd62314ebd4beafe36259973ffee2a869e5f6
Stefan Rinke <sker65@gmail.com>
2018-09-19 13:31:20 +0200

    - org imports - added looging, what gets called actually

--- src/main/java/com/rinke/solutions/pinball/view/ReflectionDispatcher.java
+++ src/main/java/com/rinke/solutions/pinball/view/ReflectionDispatcher.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.lang3.StringUtils;
