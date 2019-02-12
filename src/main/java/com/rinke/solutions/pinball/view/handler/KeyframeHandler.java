6b618c8b6a1cb59be4dca4f054e440b5a9a5b6c7
Stefan Rinke <sker65@gmail.com>
2019-02-12 14:40:59 +0100

    - added mapping

--- src/main/java/com/rinke/solutions/pinball/view/handler/KeyframeHandler.java
+++ src/main/java/com/rinke/solutions/pinball/view/handler/KeyframeHandler.java
@@ -132,6 +132,7 @@ public class KeyframeHandler extends AbstractCommandHandler implements ViewBindi
 			case COLMASK_FOLLOW: switchMode = SwitchMode.FOLLOW; break;
 			case REPLACE: switchMode = SwitchMode.REPLACE; break;
 			case LAYEREDCOL: switchMode = SwitchMode.LAYEREDCOL; break;
+			case REPLACE_FOLLOW: switchMode = SwitchMode.FOLLOWREPLACE; break;
 			case LAYEREDREPLACE: switchMode = SwitchMode.LAYEREDREPLACE; break;
 			default: break;
 		}
