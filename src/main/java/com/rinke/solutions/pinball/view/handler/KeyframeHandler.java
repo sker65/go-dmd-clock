f27e187ae1323e2b1062e771e0a17537c9e3b3dd
Joerg Amann <{ID}+{username}@users.noreply.github.com>
2020-10-11 10:44:58 +0200

    change event prefix to pipe sign

--- src/main/java/com/rinke/solutions/pinball/view/handler/KeyframeHandler.java
+++ src/main/java/com/rinke/solutions/pinball/view/handler/KeyframeHandler.java
@@ -74,7 +74,7 @@ public class KeyframeHandler extends AbstractCommandHandler implements ViewBindi
 			else return;
 			
 			if (SwitchMode.EVENT.equals(switchMode)) {
-				prompt = "§"+ namePrompt.getPrompt();
+				prompt = "|"+ namePrompt.getPrompt();
 			}
 			
 			if( vm.keyframes.containsKey(prompt) ) {
