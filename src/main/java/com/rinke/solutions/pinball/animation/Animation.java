b6700ff81a634d9beb7df75b4cf434171c24fe53
Joerg Amann <{ID}+{username}@users.noreply.github.com>
2021-03-14 10:24:27 +0100

    rename to layered ReplaceMask

--- src/main/java/com/rinke/solutions/pinball/animation/Animation.java
+++ src/main/java/com/rinke/solutions/pinball/animation/Animation.java
@@ -78,9 +78,9 @@ public class Animation {
 		COLMASK("ColorMask",					true,  false, false, false, false, false, false, false), 
 		FIXED("Fixed", 							false, true,  true, true, false, false, false, false), 
 		COLMASK_FOLLOW("ColorMask Sequence", 	true,  true,  true, false, false, true,  false,  false),
-		LAYEREDCOL("ColorMask Layered", 		true,  true,  true, true, false, false,  false, true),
+		LAYEREDCOL("Layered ColorMask", 		true,  true,  true, true, false, false,  false, true),
 		REPLACE_FOLLOW("Replace Sequence",		false, true,  true, false, false, true,  true,  false),
-		LAYEREDREPLACE("Replace Masked",		false, true,  false, false, true,  true,  false, false),
+		LAYEREDREPLACE("Layered ReplaceMask",	false, true,  true, true, true,  true,  true, true),
 		;
 
 		// label to display
