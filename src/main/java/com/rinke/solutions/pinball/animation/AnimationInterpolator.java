cee1ca46d7aa8e95f19f6ea7f7233947f48bb2f5
Stefan Rinke <sker65@gmail.com>
2021-04-13 18:34:45 +0200

    - added hist compare

--- src/main/java/com/rinke/solutions/pinball/animation/AnimationInterpolator.java
+++ src/main/java/com/rinke/solutions/pinball/animation/AnimationInterpolator.java
@@ -3,6 +3,11 @@ package com.rinke.solutions.pinball.animation;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.imgproc.Imgproc;
+
 import com.rinke.solutions.pinball.model.Frame;
 import com.rinke.solutions.pinball.model.Palette;
 
@@ -22,12 +27,12 @@ public class AnimationInterpolator {
 		return null;
 	}
 
-	public CompiledAnimation interpolate(String name, CompiledAnimation src, Palette selectedPalette) {
+	public CompiledAnimation interpolate(String name, CompiledAnimation src, Palette pal) {
 		for( int i = 0; i < src.frames.size(); i++) {
 			if( src.frames.get(i).keyFrame ) {
 				for( int j = i+1; i < src.frames.size(); j++) {
 					if( src.frames.get(j).keyFrame ) {
-						List<Frame> interpolatedFrames = this.interpolateFrames(src.frames, i, j);
+						List<Frame> interpolatedFrames = this.interpolateFrames(src.frames, i, j, pal);
 					}
 				}
 			}
@@ -35,30 +40,39 @@ public class AnimationInterpolator {
 		return null;
 	}
 
-	private List<Frame> interpolateFrames(List<Frame> frames, int startFrame, int endFrame) {
+	private List<Frame> interpolateFrames(List<Frame> frames, int startFrame, int endFrame, Palette pal) {
 		int nextLowToInterpolate = startFrame+1;
 		int nextHighToInterpolate = endFrame-1;
 		List<Frame> result = new ArrayList<>();
 		while( nextHighToInterpolate-nextLowToInterpolate >= 0) {
-			float histHighDiff = this.getBestNeighborhoodKeyframe(frames.get(nextHighToInterpolate),frames.get(nextHighToInterpolate+1));
-			float histLowDiff = this.getBestNeighborhoodKeyframe(frames.get(nextLowToInterpolate-1),frames.get(nextLowToInterpolate));
+			double histHighDiff = this.getBestNeighborhoodKeyframe(frames.get(nextHighToInterpolate),frames.get(nextHighToInterpolate+1), pal);
+			double histLowDiff = this.getBestNeighborhoodKeyframe(frames.get(nextLowToInterpolate-1),frames.get(nextLowToInterpolate), pal);
 			if( histHighDiff < histLowDiff) {
-				Frame f = this.propagateFrameFromTo(frames.get(nextHighToInterpolate+1), frames.get(nextHighToInterpolate));
+				Frame f = this.propagateFrameFromTo(frames.get(nextHighToInterpolate+1), frames.get(nextHighToInterpolate), pal);
 			} else {
-				Frame f = this.propagateFrameFromTo(frames.get(nextLowToInterpolate-1),frames.get(nextLowToInterpolate));
+				Frame f = this.propagateFrameFromTo(frames.get(nextLowToInterpolate-1),frames.get(nextLowToInterpolate), pal);
 			}
 			// TODO insert frame at the right pos!!
 		}
 		return result;
 	}
 
-	private Frame propagateFrameFromTo(Frame frame, Frame frame2) {
+	private Frame propagateFrameFromTo(Frame frame, Frame frame2, Palette pal) {
 		return null;
 	}
 
-	private float getBestNeighborhoodKeyframe(Frame frame, Frame frame2) {
+	private double getBestNeighborhoodKeyframe(Frame frame1, Frame frame2, Palette pal) {
 		// done by histogram_difference_between_frames
-		return 0;
+		float[] range = {0, 256}; //the upper boundary is exclusive
+        MatOfFloat histRange = new MatOfFloat(range);
+        MatOfInt hist1 = new MatOfInt(), hist2 = new MatOfInt();
+        List<Mat> bgrPlanes1 = new ArrayList<>();
+        // TODO add converted frame1
+        Imgproc.calcHist(bgrPlanes1, new MatOfInt(0), new Mat(), null, hist1, histRange);
+        List<Mat> bgrPlanes2 = new ArrayList<>();
+        // TODO add converted frame1
+        Imgproc.calcHist(bgrPlanes2, new MatOfInt(0), new Mat(), null, hist2, histRange);
+		return Imgproc.compareHist( hist1, hist2, 3 );
 	}
 
 }
