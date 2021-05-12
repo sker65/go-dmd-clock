c37b73903e77e8e9834f9146e1460847cbe8aef9
Stefan Rinke <sker65@gmail.com>
2021-05-12 11:52:14 +0200

    - no offset when there is an actual link

--- src/main/java/com/rinke/solutions/pinball/AnimationHandler.java
+++ src/main/java/com/rinke/solutions/pinball/AnimationHandler.java
@@ -124,7 +124,7 @@ public class AnimationHandler implements Runnable {
 						int frameNo = 0;
 						if (vm.selectedScene.getActualFrame().frameLink != null) {
             				linkedAnimation = vm.recordings.get(vm.selectedScene.getActualFrame().frameLink.recordingName);
-            				frameNo = vm.selectedScene.getActualFrame().frameLink.frame + vm.linkedFrameOffset;
+            				frameNo = vm.selectedScene.getActualFrame().frameLink.frame;
             			} else {
             				linkedAnimation = vm.recordings.get(link.associatedRecordingName);
     						frameNo =  link.startFrame + actFrame + vm.linkedFrameOffset;
